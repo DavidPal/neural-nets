9a5727dac2cbc94d8fec30c1db288b9c7d639f46
David Pal <dpal@yahoo-inc.com>
2017-02-17 13:57:28 -0500

    Implement backward propagation

--- src/main/java/com/palenica/david/MatrixUtils.java
+++ src/main/java/com/palenica/david/MatrixUtils.java
@@ -12,15 +12,48 @@ public class MatrixUtils {
         }
     }
 
+    public static double[] sigmoid(double[] data) {
+        double output[] = new double[data.length];
+        for (int i = 0; i < data.length; i++) {
+            output[i] = sigmoid(data[i]);
+        }
+        return output;
+    }
+
+    public static double sigmaPrime(final double z) {
+        double s = sigmoid(z);
+        return s * (1.0 - s);
+    }
+
+    public static void applySigmaPrime(double[] data) {
+        for (int i = 0; i < data.length; i++) {
+            data[i] = sigmaPrime(data[i]);
+        }
+    }
+
     // Vector addition
     public static void addTo(final double[] a, final double[] b) {
+        if (a.length != b.length) {
+            throw new IllegalArgumentException("Vector sizes do not match.");
+        }
         for (int i = 0; i < a.length; i++) {
             a[i] += b[i];
         }
     }
 
     // Vector addition
+    public static void addTo(final double[] a, final double b) {
+        for (int i = 0; i < a.length; i++) {
+            a[i] += b;
+        }
+    }
+
+
+    // Vector addition
     public static double[] add(final double[] a, final double[] b) {
+        if (a.length != b.length) {
+            throw new IllegalArgumentException("Vector sizes do not match.");
+        }
         double[] output = new double[a.length];
         for (int i = 0; i < a.length; i++) {
             output[i] = a[i] + b[i];
@@ -30,13 +63,26 @@ public class MatrixUtils {
 
     // Matrix addition
     public static void addTo(final double[][] A, final double[][] B) {
+        if (A.length != B.length) {
+            throw new IllegalArgumentException("Number of rows in matrices do not match.");
+        }
         for (int i = 0; i < A.length; i++) {
             addTo(A[i], B[i]);
         }
     }
 
     // Matrix addition
+    public static void addTo(final double[][] A, final double b) {
+        for (int i = 0; i < A.length; i++) {
+            addTo(A[i], b);
+        }
+    }
+
+    // Matrix addition
     public static double[][] add(final double[][] A, final double[][] B) {
+        if (A.length != B.length) {
+            throw new IllegalArgumentException("Number of rows in matrices do not match.");
+        }
         double[][] output = new double[A.length][];
         for (int i = 0; i < A.length; i++) {
             output[i] = add(A[i], B[i]);
@@ -46,6 +92,9 @@ public class MatrixUtils {
 
     // Dot product
     public static double dot(final double[] a, final double[] b) {
+        if (a.length != b.length) {
+            throw new IllegalArgumentException("Vector sizes do not match.");
+        }
         double output = 0.0;
         for (int i = 0; i < a.length; i++) {
             output += a[i] * b[i];
@@ -53,11 +102,45 @@ public class MatrixUtils {
         return output;
     }
 
-    // Matrix-vector multiplication
+    // Matrix column-vector multiplication
     public static double[] multiply(final double[][] A, final double[] b) {
         double[] output = new double[A.length];
         for (int i = 0; i < A.length; i++) {
             output[i] = dot(A[i], b);
+        }
+        return output;
+    }
+
+    // Row-vector matrix multiplication
+    public static double[] multiply(final double[] a, final double[][] B) {
+        if (a.length != B.length) {
+            throw new IllegalArgumentException("Vector size does not match number of rows of a matrix.");
+        }
+        double[] output = new double[B[0].length];
+        for (int j = 0; j < B[0].length; j++) {
+            output[j] = 0.0;
+            for (int i = 0; i < a.length; i++) {
+                output[j] += a[i] * B[i][j];
+            }
+        }
+        return output;
+    }
+
+    // Vector multiplication
+    public static void multiply(final double a, final double[] b) {
+        for (int i = 0; i < b.length; i++) {
+            b[i] *= a;
+        }
+    }
+
+    // Componentwise multiplication of vector
+    public static double[] componentMultiply(final double a[], final double[] b) {
+        if (a.length != b.length) {
+            throw new IllegalArgumentException("Vector sizes do not match.");
+        }
+        double[] output = new double[a.length];
+        for (int i = 0; i < a.length; i++) {
+            output[i] = a[i] * b[i];
         }
         return output;
     }
--- src/main/java/com/palenica/david/NeuralNetwork.java
+++ src/main/java/com/palenica/david/NeuralNetwork.java
@@ -16,148 +16,247 @@ public class NeuralNetwork {
 
     public int sizeHiddenLayer;
 
-    public double[][] firstWeights;
-    public double[] firstBias;
+    private Parameters parameters;
 
-    public double[][] secondWeights;
-    public double[] secondBias;
-
-    public static class Gradient {
+    public static class Parameters {
         final public double[][] firstWeights;
         final public double[] firstBias;
-
         final public double[][] secondWeights;
         final public double[] secondBias;
 
-        private Gradient(final double[][] firstWeights, final double[] firstBias,
-                         final double[][] secondWeights, final double[] secondBias) {
+        private Parameters(final double[][] firstWeights, final double[] firstBias,
+                           final double[][] secondWeights, final double[] secondBias) {
             this.firstWeights = firstWeights;
             this.firstBias = firstBias;
             this.secondWeights = secondWeights;
             this.secondBias = secondBias;
         }
 
-        private Gradient(final int sizeHiddenLayer) {
-            this(new double[inputSize][sizeHiddenLayer], new double[sizeHiddenLayer],
-                    new double[sizeHiddenLayer][outputSize], new double[outputSize]);
+        private Parameters(final int sizeHiddenLayer) {
+            this(new double[sizeHiddenLayer][inputSize],
+                 new double[sizeHiddenLayer],
+                 new double[outputSize][sizeHiddenLayer],
+                 new double[outputSize]);
         }
 
-        private void add(final Gradient other) {
+        public static Parameters createDeepCopy(final Parameters parameters) {
+            final int sizeHiddenLayer = parameters.firstBias.length;
+            final double[][] firstWeights = new double[sizeHiddenLayer][inputSize];
+            final double[] firstBias = new double[sizeHiddenLayer];
+            final double[][] secondWeights = new double[outputSize][sizeHiddenLayer];
+            final double[] secondBias = new double[outputSize];
+
+            for (int i = 0; i < sizeHiddenLayer ; i++) {
+                for (int j = 0; j < inputSize; j++) {
+                    firstWeights[i][j] = parameters.firstWeights[i][j];
+                }
+            }
+
+            for (int i = 0; i < sizeHiddenLayer; i++) {
+                firstBias[i] = parameters.firstBias[i];
+            }
+
+            for (int i = 0; i < outputSize; i++) {
+                for (int j = 0; j < sizeHiddenLayer; j++) {
+                    secondWeights[i][j] = parameters.secondWeights[i][j];
+                }
+            }
+
+            for (int i = 0; i < outputSize; i++) {
+                secondBias[i] = parameters.secondBias[i];
+            }
+
+            return new Parameters(firstWeights, firstBias, secondWeights, secondBias);
+        }
+
+        public static Parameters loadParametersFromFile(final String fileName) throws IOException {
+            Path filePath = Paths.get(fileName);
+            Scanner scanner = new Scanner(filePath);
+
+            final int sizeHiddenLayer = scanner.nextInt();
+
+            final double[][] firstWeights = new double[sizeHiddenLayer][inputSize];
+            final double[] firstBias = new double[sizeHiddenLayer];
+            final double[][] secondWeights = new double[outputSize][sizeHiddenLayer];
+            final double[] secondBias = new double[outputSize];
+
+            for (int i = 0; i < sizeHiddenLayer ; i++) {
+                for (int j = 0; j < inputSize; j++) {
+                    firstWeights[i][j] = scanner.nextDouble();
+                }
+            }
+
+            for (int i = 0; i < sizeHiddenLayer; i++) {
+                firstBias[i] = scanner.nextDouble();
+            }
+
+            for (int i = 0; i < outputSize; i++) {
+                for (int j = 0; j < sizeHiddenLayer; j++) {
+                    secondWeights[i][j] = scanner.nextDouble();
+                }
+            }
+
+            for (int i = 0; i < outputSize; i++) {
+                secondBias[i] = scanner.nextDouble();
+            }
+
+            return new Parameters(firstWeights, firstBias, secondWeights, secondBias);
+        }
+
+        private void add(final Parameters other) {
             MatrixUtils.addTo(firstWeights, other.firstWeights);
             MatrixUtils.addTo(firstBias, other.firstBias);
-
             MatrixUtils.addTo(secondWeights, other.secondWeights);
             MatrixUtils.addTo(secondBias, other.secondBias);
+        }
+
+        public void saveToFile(final String fileName) throws FileNotFoundException, UnsupportedEncodingException {
+            final int sizeHiddenLayer = firstBias.length;
+            PrintWriter writer = new PrintWriter(fileName, "UTF-8");
+            writer.println(sizeHiddenLayer);
+
+            for (int i = 0; i < sizeHiddenLayer; i++) {
+                for (int j = 0; j < inputSize; j++) {
+                    writer.print(String.format(java.util.Locale.US, "%15.9f ", firstWeights[i][j]));
+                }
+                writer.println();
+            }
+            writer.println();
+
+            for (int k = 0; k < sizeHiddenLayer; k++) {
+                writer.print(String.format(java.util.Locale.US, "%15.9f ", firstBias[k]));
+            }
+            writer.println();
+            writer.println();
+
+            for (int i = 0; i < outputSize; i++) {
+                for (int j = 0; j < sizeHiddenLayer; j++) {
+                    writer.print(String.format(java.util.Locale.US, "%15.9f ", secondWeights[i][j]));
+                }
+                writer.println();
+            }
+            writer.println();
+
+
+            for (int j = 0; j < outputSize; j++) {
+                writer.print(String.format(java.util.Locale.US, "%15.9f ", secondBias[j]));
+            }
+            writer.println();
+            writer.println();
+            writer.close();
         }
     }
 
     public NeuralNetwork(final int sizeHiddenLayer) {
         this.sizeHiddenLayer = sizeHiddenLayer;
-        this.firstWeights = new double[sizeHiddenLayer][inputSize];
-        this.firstBias = new double[sizeHiddenLayer];
-        this.secondWeights = new double[outputSize][sizeHiddenLayer];
-        this.secondBias = new double[outputSize];
+        this.parameters = new Parameters(sizeHiddenLayer);
     }
 
-    public void InitializeParameters(final long seed) {
-        Random random = new Random(seed);
-        for (int i = 0; i < sizeHiddenLayer; i++) {
-            for (int j = 0; j < inputSize; j++) {
-                firstWeights[i][j] = random.nextGaussian() / Math.sqrt(inputSize);
-            }
-        }
-
-        for (int i = 0; i < sizeHiddenLayer; i++) {
-            firstBias[i] = random.nextGaussian();
-        }
-
-        for (int i = 0; i < outputSize; i++) {
-            for (int j = 0; j < sizeHiddenLayer; j++) {
-                secondWeights[i][j] = random.nextGaussian() / Math.sqrt(sizeHiddenLayer);
-            }
-        }
-
-        for (int i = 0; i < outputSize; i++) {
-            secondBias[i] = random.nextGaussian();
-        }
+    public void loadParametersFromFile(final String fileName) throws IOException {
+        this.parameters = Parameters.loadParametersFromFile(fileName);
+        this.sizeHiddenLayer = parameters.firstBias.length;
     }
 
     public void saveToFile(final String fileName) throws FileNotFoundException, UnsupportedEncodingException {
-        PrintWriter writer = new PrintWriter(fileName, "UTF-8");
-        writer.println(sizeHiddenLayer);
-
-        for (int i = 0; i < sizeHiddenLayer; i++) {
-            for (int j = 0; j < inputSize; j++) {
-                writer.print(String.format(java.util.Locale.US, "%15.9f ", firstWeights[i][j]));
-            }
-            writer.println();
-        }
-        writer.println();
-
-        for (int k = 0; k < sizeHiddenLayer; k++) {
-            writer.print(String.format(java.util.Locale.US, "%15.9f ", firstBias[k]));
-        }
-        writer.println();
-        writer.println();
-
-        for (int i = 0; i < outputSize; i++) {
-            for (int j = 0; j < sizeHiddenLayer; j++) {
-                writer.print(String.format(java.util.Locale.US, "%15.9f ", secondWeights[i][j]));
-            }
-            writer.println();
-        }
-        writer.println();
-
-
-        for (int j = 0; j < outputSize; j++) {
-            writer.print(String.format(java.util.Locale.US, "%15.9f ", secondBias[j]));
-        }
-        writer.println();
-        writer.println();
-        writer.close();
+        parameters.saveToFile(fileName);
     }
 
-    public void loadFromFile(final String fileName) throws IOException {
-        Path filePath = Paths.get(fileName);
-        Scanner scanner = new Scanner(filePath);
+    private double[] computeHidden(final double[] input) {
+        final double[] hidden = MatrixUtils.multiply(parameters.firstWeights, input);
+        MatrixUtils.addTo(hidden, parameters.firstBias);
+        MatrixUtils.applySigmoid(hidden);
+        return hidden;
+    }
 
-        this.sizeHiddenLayer = scanner.nextInt();
-
-        for (int i = 0; i < sizeHiddenLayer ; i++) {
-            for (int j = 0; j < inputSize; j++) {
-                firstWeights[i][j] = scanner.nextDouble();
-            }
-        }
-
-        for (int k = 0; k < sizeHiddenLayer; k++) {
-            firstBias[k] = scanner.nextDouble();
-        }
-
-        for (int i = 0; i < outputSize; i++) {
-            for (int j = 0; j < sizeHiddenLayer; j++) {
-                secondWeights[i][j] = scanner.nextDouble();
-            }
-        }
-
-        for (int j = 0; j < outputSize; j++) {
-            secondBias[j] = scanner.nextDouble();
-        }
+    private double[] computeOutput(final double[] hidden) {
+        final double[] output = MatrixUtils.multiply(parameters.secondWeights, hidden);
+        MatrixUtils.addTo(output, parameters.secondBias);
+        MatrixUtils.applySigmoid(output);
+        return output;
     }
 
     public double[] predict(final double[] input) {
-        double[] result1 = MatrixUtils.multiply(firstWeights, input);
-        MatrixUtils.addTo(result1, firstBias);
-        MatrixUtils.applySigmoid(result1);
-
-        double[] result2 = MatrixUtils.multiply(secondWeights, input);
-        MatrixUtils.addTo(result2, secondBias);
-        MatrixUtils.applySigmoid(result2);
-
-        return result2;
+        double[] hidden = computeHidden(input);
+        return computeOutput(hidden);
     }
 
-    public Gradient getGradient(final double[][] image, final double L2regularization) {
-        return new Gradient(sizeHiddenLayer);
+    public void initializeParameters(final long seed) {
+        Random random = new Random(seed);
+        for (int i = 0; i < sizeHiddenLayer; i++) {
+            for (int j = 0; j < inputSize; j++) {
+                parameters.firstWeights[i][j] = random.nextGaussian() / Math.sqrt(inputSize);
+            }
+        }
+
+        for (int i = 0; i < sizeHiddenLayer; i++) {
+            parameters.firstBias[i] = random.nextGaussian();
+        }
+
+        for (int i = 0; i < outputSize; i++) {
+            for (int j = 0; j < sizeHiddenLayer; j++) {
+                parameters.secondWeights[i][j] = random.nextGaussian() / Math.sqrt(sizeHiddenLayer);
+            }
+        }
+
+        for (int i = 0; i < outputSize; i++) {
+            parameters.secondBias[i] = random.nextGaussian();
+        }
+    }
+
+    // See documentation in "doc/" directory.
+    public Parameters getGradient(final double[] input, final double[] label) {
+        final double[] preHidden = MatrixUtils.multiply(parameters.firstWeights, input);
+        MatrixUtils.addTo(preHidden, parameters.firstBias);
+        final double[] hidden = MatrixUtils.sigmoid(preHidden);
+        double[] output = computeOutput(hidden);
+
+        // This overwrites up both output and label. Beware!
+        MatrixUtils.multiply(-1.0, output);
+        MatrixUtils.addTo(label, output);
+        double[] gradientSecondBias = label;
+
+        double[][] gradientSecondWeights = new double[outputSize][sizeHiddenLayer];
+        for (int i = 0; i < outputSize; i++) {
+            for (int j = 0; j < sizeHiddenLayer; j++) {
+                gradientSecondWeights[i][j] = label[i] * hidden[j];
+            }
+        }
+
+        MatrixUtils.applySigmaPrime(preHidden);
+        double[] hiddenError = MatrixUtils.multiply(label, parameters.secondWeights);
+        double[] gradientFirstBias = MatrixUtils.componentMultiply(hiddenError, preHidden);
+
+        double[][] gradientFirstWeights = new double[sizeHiddenLayer][inputSize];
+        for (int i = 0; i < sizeHiddenLayer; i++) {
+            for (int j = 0; j < inputSize; j++) {
+                gradientFirstWeights[i][j] = gradientFirstBias[i] * input[j];
+            }
+        }
+
+        return new Parameters(gradientFirstWeights, gradientFirstBias, gradientSecondWeights, gradientSecondBias);
+    }
+
+    public void updateParameters(final double[][] inputs, final double[][] labels, final double stepSize, final double L2regularization) {
+        Parameters parameters = Parameters.createDeepCopy(this.parameters);
+
+        final double[][] firstWeights = new double[sizeHiddenLayer][inputSize];
+        final double[] firstBias = new double[sizeHiddenLayer];
+        final double[][] secondWeights = new double[outputSize][sizeHiddenLayer];
+        final double[] secondBias = new double[outputSize];
+
+        MatrixUtils.addTo(firstWeights, L2regularization);
+        MatrixUtils.addTo(firstBias, L2regularization);
+        MatrixUtils.addTo(secondWeights, L2regularization);
+        MatrixUtils.addTo(secondBias, L2regularization);
+
+        parameters.add(new Parameters(firstWeights, firstBias, secondWeights, secondBias));
+
+        for (int i = 0; i < inputs.length; i++) {
+            Parameters gradient = getGradient(inputs[i], labels[i]);
+            parameters.add(gradient);
+        }
+
+        this.parameters = parameters;
     }
 
 }
